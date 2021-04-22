7d5e47cb92a5f92603ed895f03e0f4432121eeaf
zxxzyyz <zxxzyyz2018@gmail.com>
2021-04-22 11:56:37 +0900

    Add a command for log reader

---
+++ GloryChanger_Commands/Commands/Cmd3D.cs
@@ -0,0 +1,49 @@
+﻿namespace GloryChanger_Commands.Commands
+{
+    /// <summary>ログリード</summary>
+    public class Cmd3D : Command
+    {
+        protected override byte DH1 { get; } = 0x3D;
+
+        public enum Cmd3DConfig
+        {
+            /// <summary>本体部保守ログ</summary>
+            Config1 = 1,
+            /// <summary>本体部点検ログ</summary>
+            Config2 = 2,
+            /// <summary>本体部データログ</summary>
+            Config3 = 3,
+            /// <summary>エラー時系列ログ</summary>
+            Config4 = 4,
+            /// <summary>処理再現ログ</summary>
+            Config5 = 10,
+            /// <summary>日別計数ログ</summary>
+            Config6 = 11,
+            /// <summary>硬貨部保守ログ</summary>
+            Config7 = 41,
+            /// <summary>硬貨部点検ログ</summary>
+            Config8 = 42,
+            /// <summary>硬貨部エラー時系列ログ</summary>
+            Config9 = 44,
+            /// <summary>紙幣部保守ログ</summary>
+            Config10 = 81,
+            /// <summary>紙幣部点検ログ</summary>
+            Config11 = 82,
+            /// <summary>紙幣部エラー時系列ログ</summary>
+            Config12 = 84
+        }
+
+        public void SetData(Cmd3DConfig config, string index)
+        {
+            var idx = index.PadLeft(4, '0');
+            var temp = new System.Collections.Generic.List<byte>();
+            temp.Add(((int)config).GetByteAtDigit(2));
+            temp.Add(((int)config).GetByteAtDigit(1));
+            temp.Add((byte)(System.Convert.ToByte(idx.Substring(0, 1), 16) + 48));
+            temp.Add((byte)(System.Convert.ToByte(idx.Substring(1, 1), 16) + 48));
+            temp.Add((byte)(System.Convert.ToByte(idx.Substring(2, 1), 16) + 48));
+            temp.Add((byte)(System.Convert.ToByte(idx.Substring(3, 1), 16) + 48));
+            Data = temp.ToArray();
+        }
+    }
+}
--- GloryChanger_Commands/Extensions.cs
+++ GloryChanger_Commands/Extensions.cs
@@ -11,6 +11,7 @@ namespace GloryChanger_Commands
             foreach (byte b in data) sb.Append(Convert.ToString(b, 16).PadLeft(2, '0').PadRight(3, ' '));
             return sb.ToString().ToUpper();
         }
+
         public static byte GetByteAtDigit(this int value, int digit, int addValue = 48)
         {
             var temp = value.ToString().PadLeft(digit, '0');
@@ -23,6 +24,30 @@ namespace GloryChanger_Commands
             var temp = value.ToString("X").PadLeft(digit, '0');
             var intValue = int.Parse(temp.Substring(temp.Length - digit, 1), System.Globalization.NumberStyles.HexNumber);
             return (byte)(intValue + addValue);
+        }
+
+        public static void AddEachChar(this System.Collections.Generic.List<byte> list, string text, int length, bool fromFirst, bool toASCII)
+        {
+            for (int i = 0; i < length; i++)
+            {
+                try
+                {
+                    if (toASCII)
+                    {
+                        if (fromFirst) list.Add((byte)text[i]);
+                        else list.Add((byte)text[text.Length - i - 1]);
+                    }
+                    else
+                    {
+                        if (fromFirst) list.Add(byte.Parse(text.Substring(i, 1)));
+                        else list.Add(byte.Parse(text.Substring(text.Length - i - 1, 1)));
+                    }
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    list.Add(0);
+                }
+            }
         }
     }
 }
